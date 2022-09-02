0907e6cf113ac4bfed5d94d262f64e5ac085fdf1
Alexandre Machado <alexandre.machado@neon.com.br>
2022-09-02 17:23:13 -0300

    coisas

---
+++ SudokuSolver/Cell.cs
@@ -0,0 +1,60 @@
+﻿namespace SudokuSolver;
+
+public class Cell
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public int Value { get; set; }
+    public bool IsInitial { get; set; }
+    public bool[] PencilValues { get; set; }
+
+    public void InitPencilValues()
+    {
+        for (var i = 0; i < PencilValues.Length; i++)
+            PencilValues[i] = true;
+    }
+
+    public bool HasOnlyOnePossibility
+    {
+        get
+        {
+            var x = 0;
+            foreach (var value in PencilValues)
+                if (value) x++;
+            return x == 1;
+        }
+    }
+
+    public int GetPossibility()
+    {
+        for (var i = 0; i < PencilValues.Length; i++)
+        {
+            if (!PencilValues[i]) continue;
+            return i;
+        }
+        return 0;
+    }
+
+    public int Group
+    {
+        get
+        {
+            var g = 1;
+            if (((float)X / 3) > 1) g++;
+            if (((float)X / 3) > 2) g++;
+            if (((float)Y / 3) > 1) g += 3;
+            if (((float)Y / 3) > 2) g += 3;
+            return g;
+        }
+    }
+
+    public Cell(int x, int y, int value)
+    {
+        PencilValues = new bool[10];
+        X = x;
+        Y = y;
+        Value = value;
+        IsInitial = value > 0;
+    }
+}
---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SudokuSolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var board = new Board("530070000600195000098000060800060003400803001700020006060000280000419005000080079");
+            //var board = new Board("209000600040870012800019040030700801065008030100030007000650709604000020080301450");
+            var board = new Board("078009500005200030000000004000080000620000070000003600700016000819500006003408000");
+
+            while (true)
+            {
+                board.Print();
+                board.Step();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
